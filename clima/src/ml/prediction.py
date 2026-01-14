256416a91fa0bea4ff033a7e1e2a25209e8985a8
Eveliz08 <espinaco.eveliz@gmail.com>
2026-01-14 09:52:58 -0500

    Add docstring for prediction module outlining input format and prediction goals

---
+++ clima/src/ml/prediction.py
@@ -0,0 +1,6 @@
+"""
+Docstring for clima.src.ml.prediction
+La entrada es un vector con los campos día, mes, año, cuidad, país (algunos pueden estar vacíos y se debe manejar esto)
+El objetivo es predecir temperatura y alertar si puede ocurrir algún evento climático extremo: ola de calor o de frío
+
+"""
